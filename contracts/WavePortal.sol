74efaf89202186fb3257290b1527ea7d8ba57d42
cryptichic <syberchief@gmail.com>
2022-07-07 16:37:12 -0500

    added the wave functionality

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,14 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    
     constructor() {
         console.log("Hey! I'm a contract and smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
     }
 }
