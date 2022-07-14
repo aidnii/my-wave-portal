8da6a10e45f16ad0d95153fe5a2ebb94b472c4f9
cryptichic <syberchief@gmail.com>
2022-07-13 19:45:31 -0500

    updating the run.js file

--- scripts/run.js
+++ scripts/run.js
@@ -1,21 +1,26 @@
 const main = async () => {
     
     const kissContractFactory = await hre.ethers.getContractFactory("kissingPortal");
-    const kissContract = await kissContractFactory.deploy();
+    const kissContract = await kissContractFactory.deploy({
+      value: hre.ethers.utils.parseEther("0.1"),
+    });
     await kissContract.deployed();
   
     console.log("Contract address:", kissContract.address);
   
-    let kissCount;
-    kissCount = await kissContract.getTotalKisses();
-    console.log(kissCount.toNumber());
+    //Get contract balance
+    let contractBalance = await hre.ethers.provider.getBalance(
+      kissContract.address
+    );
+    console.log("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
   
+    //send kisses
     let kissTxn = await kissContract.kiss("A message!");
     await kissTxn.wait();
 
-    const [_, randomPerson] = await hre.ethers.getSigners();
-    kissTxn = await kissContract.connect(randomPerson).kiss("Another message!");
-    await kissTxn.wait();
+    //Get Contract Balance to see what happened!
+    contractBalance = await hre.ethers.provider.getBalance(kissContract.address);
+    console.log("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
   
     let allKisses = await kissContract.getAllKisses();
     console.log(allKisses);
